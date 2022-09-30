9464804c56325b7518cbe64a4845449eb23ca7e1
Aman9641 <amanviratjha001@gmail.com>
2022-09-30 21:00:48 +0530

    Initial version

---
+++ src/com/ATM.java
@@ -0,0 +1,18 @@
+package com;
+
+public class ATM {
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+
+    //default constructor
+    public ATM(){
+
+
+    }
+
+
+    //getter setter for each methods
+
+
+}
